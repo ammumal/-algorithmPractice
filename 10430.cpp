eb2568034ba1e1756578b4671d345ce3db117497
Lee Jieun <58466461+ammumal@users.noreply.github.com>
2021-01-18 21:54:08 +0900

    Add files via upload
    수학 관련 코드 연습 DAY 1& DAY2

---
+++ 10430.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c = 0;
+
+    cin >> a >> b >> c;
+    cout << (A+B)%C << endl 
+    << ((A%C) + (B%C))%C << endl 
+    << (A×B)%C << endl
+    << ((A%C) × (B%C))%C << endl;
+
+    return 0;
+}
